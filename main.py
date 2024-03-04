6b536a97c488d89cb41eed61f4a5a2d22a5f3c9d
mariosilva81 <mariosilva81@icloud.com>
2024-03-04 17:57:47 -0300

    resposta 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Resposta 1:
+# RESPOSTA 1:
 
 INDICE = 13
 SOMA = 0
@@ -10,4 +10,37 @@ while K < INDICE:
 
 print(SOMA)
 
-# Resposta 2:
+
+# ================================
+
+# RESPOSTA 2:
+
+# Obtém o número informado pelo usuário.
+number = int(
+    input("Digite um número para verificar se ele pertence à sequência de Fibonacci: ")
+)
+
+
+# Cria a sequência de Fibonacci até o número informado pelo usuário.
+def fibonacci(number):
+    fib_sequence = [0, 1]
+    while fib_sequence[-1] < number:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+
+fibonacci_sequence = fibonacci(number)
+
+
+# Verifica se o número informado pertence à sequência de Fibonacci.
+def fibonacci_verify(number, sequence):
+    if number in sequence:
+        print(f"O número {number} pertence à sequência de Fibonacci.")
+    else:
+        print(f"O número {number} não pertence à sequência de Fibonacci.")
+
+
+fibonacci_verify(number, fibonacci_sequence)
+
+
+# ================================
